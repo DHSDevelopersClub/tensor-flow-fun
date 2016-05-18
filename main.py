4b26e125f7ae179a4b36fef4c52dcba753c7705d
duncangrubbs <duncan.grubbs@gmail.com>
2016-05-18 12:34:49 -0700

    Add imports.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import pandas
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.utils import check_array
+from sklearn.cross_validation import train_test_split
+import tensorflow as tf
+import tensorflow.contrib.learn as skflow
+
+
+
